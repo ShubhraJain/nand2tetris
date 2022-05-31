f64b757585257f45f4dbbc50f4b5fb56e8b0f68f
s0j029r <Shubhra.Jain0@walmartlabs.com>
2022-05-30 21:55:35 -0700

    create SymbolAttributes class and fix SymbolTable methods

---
+++ projects/11/src/SymbolAttributes.java
@@ -0,0 +1,5 @@
+public class SymbolAttributes {
+  public String type;
+  public String kind;
+  public int index;
+}
--- projects/11/src/SymbolTable.java
+++ projects/11/src/SymbolTable.java
@@ -1,16 +1,16 @@
 import java.util.Hashtable;
 
 public class SymbolTable {
-  private Hashtable<String, Object[]> classSymbols;
-  private Hashtable<String, Object[]> subroutineSymbols;
+  private Hashtable<String, SymbolAttributes> classSymbols;
+  private Hashtable<String, SymbolAttributes> subroutineSymbols;
   private int lclCount;
   private int staticCount;
   private int fieldCount;
   private int argCount;
 
   public SymbolTable() {
-    this.classSymbols = new Hashtable<String, Object[]>();
-    this.subroutineSymbols = new Hashtable<String, Object[]>();
+    this.classSymbols = new Hashtable<String, SymbolAttributes>();
+    this.subroutineSymbols = new Hashtable<String, SymbolAttributes>();
     lclCount = 0;
     staticCount = 0;
     fieldCount = 0;
@@ -24,28 +24,28 @@ public class SymbolTable {
   }
 
   public void define(String name, String type, String kind) {
-    Object[] values = new Object[3];
-    values[0] = type;
-    values[1] = kind;
+    SymbolAttributes attributes = new SymbolAttributes();
+    attributes.type = type;
+    attributes.kind = kind;
     switch (kind) {
       case "arg":
-        values[2] = argCount;
-        subroutineSymbols.put(name, values);
+        attributes.index = argCount;
+        subroutineSymbols.put(name, attributes);
         argCount++;
         break;
       case "var":
-        values[2] = lclCount;
-        subroutineSymbols.put(name, values);
+        attributes.index = lclCount;
+        subroutineSymbols.put(name, attributes);
         lclCount++;
         break;
       case "static":
-        values[2] = staticCount;
-        classSymbols.put(name, values);
+        attributes.index = staticCount;
+        classSymbols.put(name, attributes);
         staticCount++;
         break;
       case "field":
-        values[2] = fieldCount;
-        classSymbols.put(name, values);
+        attributes.index = fieldCount;
+        classSymbols.put(name, attributes);
         fieldCount++;
         break;
     }
@@ -72,26 +72,26 @@ public class SymbolTable {
 
   public String kindOf(String name) {
     if (subroutineSymbols.containsKey(name)) {
-      return subroutineSymbols.get(name)[1].toString();
+      return subroutineSymbols.get(name).kind;
     } else if (classSymbols.containsKey(name)) {
-      return classSymbols.get(name)[1].toString();
+      return classSymbols.get(name).kind;
     } 
     return "NONE";
   }
 
   public String typeOf(String name) {
     if (subroutineSymbols.containsKey(name)) {
-      return subroutineSymbols.get(name)[0].toString();
+      return subroutineSymbols.get(name).type;
     }
-    return classSymbols.get(name)[0].toString();
+    return classSymbols.get(name).type;
   }
 
   public int indexOf(String name) {
     if (subroutineSymbols.containsKey(name)) {
-      return (Integer) subroutineSymbols.get(name)[2];
+      return subroutineSymbols.get(name).index;
     }
     if (classSymbols.containsKey(name)) {
-      return (Integer) classSymbols.get(name)[2];
+      return classSymbols.get(name).index;
     }
     return 0;
   }
